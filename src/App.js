44dd08e6fd502c3fcfbb843c53a44d5a13088a14
sean <seankim21@naver.com>
2021-02-22 14:45:49 +0900

    ReactJs-2.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Potato from "./potato";
+
+function Food({ fav }) {
+  return <h1>I love {fav}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!!!!!!!!</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="codes" />
+      <Food fav="garaage" />
+      <Food fav="chicken" />
     </div>
   );
 }
